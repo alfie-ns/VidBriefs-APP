4357abc1c9b7bd407fdcb5777a4aa3715f034bd5
alfie-ns <alfienurse@gmail.com>
2024-07-29 03:13:33 +0100

    Set openai-apikey to ??? before push

--- get-key.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-open -a "Google Chrome" "https://platform.openai.com/api-keys"
-
-# Define the bold text escape code
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
-
-# Multi-line echo with bold text(-e flag to interpret backslashes(\n))
-echo -e "\n${BOLD}Enter API key into: vidbriefs-app/vidbriefs-final.xcodeproj/xcshareddate/xcschemes/vidbriefs-final.xcscheme${NORMAL}\n"
